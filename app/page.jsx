dd34074f5b468c9e2c0b62dba6ab034c11667f19
Nathan0769 <nathanrivollier69@gmail.com>
2025-07-10 11:03:26 +0200

    init projet

---
+++ app/page.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-4 py-8 max-w-4xl m-auto px-4">
+      <header className="flex items-center justify-between relative">
+        <h1 className="text-4xl font-bold absolute left-1/2 -translate-x-1/2 w-full text-center">
+          Movie Finder
+        </h1>
+        <div className="flex-1"></div>
+        <label className="flex cursor-pointer gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <input
+            type="checkbox"
+            value="dark"
+            className="toggle theme-controller"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
+      </header>
+      <main></main>
+    </div>
+  );
+}
